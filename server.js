8038e62cdc49b87ed80484679225d489a1cb15df
fernando <fernandohugo097@gmail.com>
2025-09-29 15:24:53 -0600

    no se xd

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ app.use(express.json());
 
 // Ruta de prueba
 app.get('/', (req, res) => {
-  res.send('🚀 Servidor Express funcionando!');
+  res.send('🚀 Servidor Express funcionando! Angel te amo chiquito bb hermoso');
 });
 
 // Puerto de conexión
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log(`Servidor corriendo en algun lugar http://localhost:${PORT}`);
+  console.log(`Servidor corriendo en algun lugar de tu corazon miamor http://localhost:${PORT}`);
 });
