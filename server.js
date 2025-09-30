453046f240c10e1f6e3469e638a45388fc683e4b
fernando <fernandohugo097@gmail.com>
2025-09-29 18:15:32 -0600

    ejemplo de base de datos creo

--- server.js
+++ server.js
@@ -1,16 +1,29 @@
-const express = require('express');
+const express = require("express");
+const { Pool } = require("pg");
+
 const app = express();
+const port = 3000;
 
-// Middleware para JSON
-app.use(express.json());
-
-// Ruta de prueba
-app.get('/', (req, res) => {
-  res.send('🚀 Servidor Express funcionando! Angel te amo chiquito bb hermoso');
+// Configuración de la conexión a PostgreSQL
+const pool = new Pool({
+  user: "postgres",       // 👈 cámbialo
+  host: "localhost",
+  database: "ejemplo_db",   // 👈 cámbialo
+  password: "12345",  // 👈 cámbialo
+  port: 5432,
 });
 
-// Puerto de conexión
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor corriendo en algun lugar de tu corazon miamor http://localhost:${PORT}`);
+// Ruta para probar la conexión
+app.get("/", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+    res.send(`Conexión exitosa. Hora en DB: ${result.rows[0].now}`);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error conectando a la base de datos");
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
 });
